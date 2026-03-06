e8dce581e8f86f561bf5fa8b0c6e3f2e953be55e
PedroMesquita97 <pedrohenriquethx@gmail.com>
2026-03-06 17:07:37 -0300

    alteração 1

--- exercicio2.py
+++ exercicio2.py
@@ -11,9 +11,9 @@ def rodar_dado():
     dado_num = random.randint(1,6)
     label_numero.configure(text=str(dado_num))
     
-
 label_numero = ctk.CTkLabel(app, text="0", font=("Arial", 70, "bold"))
 label_numero.pack(pady=20)
+
 
 botao_clicar = ctk.CTkButton(app, text="Gire o dado", command=rodar_dado)
 botao_clicar.pack(pady=20)
